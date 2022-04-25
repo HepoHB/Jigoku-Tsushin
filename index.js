cc75675c317505b930095312a8d93dd64bdaba7a
HepoHB <NeroHaziel@outlook.com>
2022-04-25 19:08:47 -0300

    INDEX

---
+++ index.js
@@ -0,0 +1,12 @@
+/*let languages = "{ 
+    {"language": 'pt-BR', "phrase": "Sua dor será aliviada"},
+    {"language": "en-US", "phrase": "We will take revenge on your behalf"},
+    {"language": "jp-JP", "phrase": "あなたの怨み、晴らします"}
+}"
+let languages = JSON.parse(language)*/
+
+window.onload = function getLanguage(){
+    
+    console.log(window.navigator.language);
+
+}
