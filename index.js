74d5884495b9f1a2ad55e5e19687e4038d3ee908
HepoHB <NeroHaziel@outlook.com>
2022-07-22 15:35:40 -0300

    Faltando Preloader, Envio de Formulário e Bloqueio do site quando não se é 00:00.

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ const URL = "language.json";
 
 window.onload = function getLanguage(){
     $(function(){
-
         fetch(URL).then(res => res.json()).then(data =>{
             for (let index = 0; index < 3; index++) {
                 if (window.navigator.language == data.languages[index].language) {
-                    text = data.languages[index].language;
-                    document.querySelector(".text").innerHTML = data.languages[0].phrase;
-                    document.querySelector("#btn").value = data.languages[0].button;
+                    document.querySelector(".text").innerHTML = data.languages[2].phrase;
+                    document.querySelector("#btn").value = data.languages[2].button;
 
                 }
                 
             }
 
-        })
+        });
 
-    })
+    });
+
 }
