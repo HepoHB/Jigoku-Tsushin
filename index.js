6127b8c3a70e2126062f9cb45bc241abd837ad2a
HepoHB <NeroHaziel@outlook.com>
2022-07-22 00:53:10 -0300

    Good, but not Perfect

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-/*let languages = "{ 
-    {"language": 'pt-BR', "phrase": "Sua dor será aliviada"},
-    {"language": "en-US", "phrase": "We will take revenge on your behalf"},
-    {"language": "jp-JP", "phrase": "あなたの怨み、晴らします"}
-}"
-let languages = JSON.parse(language)*/
+const URL = "language.json";
 
 window.onload = function getLanguage(){
-    
-    console.log(window.navigator.language);
+    $(function(){
 
+        fetch(URL).then(res => res.json()).then(data =>{
+            for (let index = 0; index < 3; index++) {
+                if (window.navigator.language == data.languages[index].language) {
+                    text = data.languages[index].language;
+                    document.querySelector(".text").innerHTML = data.languages[0].phrase;
+                    document.querySelector("#btn").value = data.languages[0].button;
+
+                }
+                
+            }
+
+        })
+
+    })
 }
